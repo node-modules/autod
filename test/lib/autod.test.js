c5b5aa27c21e8a387ead08fe8bdfc7b513e960d2
dead_horse <dead_horse@qq.com>
2013-12-23 21:53:09 +0800

    complete write to package.json

---
+++ test/lib/autod.test.js
@@ -0,0 +1,13 @@
+/*!
+ * autod - lib/autod.js
+ * Copyright(c) 2013 
+ * Author: dead_horse <undefined>
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mm = require('mm');
+var should = require('should');
